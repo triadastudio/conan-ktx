85f430abdd2798f367dd19852c77f7d2d03d5ce3
Bagrat Dabaghyan <dbagrat@gmail.com>
2023-02-27 04:04:38 +0400

    [wip] Migrate to Conan 2.0, update to latest KTX-Software Fix Conan package/target names Remove unused stuff Add platform Conan profiles

--- profiles/linux.profile
+++ profiles/linux.profile
@@ -1,15 +1,7 @@
-include(default)
-
 [settings]
 os=Linux
+arch=x86_64
 compiler=clang
 compiler.version=13
 compiler.libcxx=libstdc++11
-
-[env]
-CC=/usr/bin/clang-13
-CXX=/usr/bin/clang++-13
-
-[conf]
-tools.system.package_manager:mode=install
-tools.system.package_manager:sudo=True
+compiler.cppstd=20
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,18 +1,26 @@
-from conans import ConanFile, CMake, tools
-import os
+from conan import ConanFile
+from conan.tools import cmake, build
 
+import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake.cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+        cm = cmake.CMake(self)
+        cm.configure()
+        cm.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "test_package")
+        if build.can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
             ktx_path = os.path.join(self.source_folder, "etc1.ktx")
-            self.run("{} {}".format(bin_path, ktx_path), run_environment=True)
+            self.run("{} {}".format(bin_path, ktx_path), env="conanrun")
