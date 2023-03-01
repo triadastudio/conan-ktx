f559d56e90f048bb141a53b06a541b0f89e9d4b5
Bagrat Dabaghyan <dbagrat@gmail.com>
2023-03-01 18:26:10 +0400

    [upd-1] Fix Linux profile, fix MacOS build

--- conanfile.py
+++ conanfile.py
@@ -96,8 +96,8 @@ class KtxConan(ConanFile):
         self.cpp_info.components["libktx"].names["cmake_find_package"] = "ktx"
         self.cpp_info.components["libktx"].names["cmake_find_package_multi"] = "ktx"
         self.cpp_info.components["libktx"].libs = ["ktx"]
-        if self.settings.arch == "x86_64":
-            self.cpp_info.components["libktx"].libs.append("astcenc-avx2-static")
+        # if self.settings.arch == "x86_64":
+        #     self.cpp_info.components["libktx"].libs.append("astcenc-avx2-static")
         self.cpp_info.components["libktx"].defines = [
             "KTX_FEATURE_KTX1", "KTX_FEATURE_KTX2", "KTX_FEATURE_WRITE"
         ]
