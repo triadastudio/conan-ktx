abd5f70f9971fa6fabb18af0f9c564ab7d6b22bf
SenPie <smbat@duck.com>
2024-09-12 02:22:55 +0200

    [4.3.2b] Update KTX Library, patch older VC runtimes compatiblity
    Update support for 4.3.2 ktx release.
    Patch older Visual C++ compatiblity by disabling constexpr mutex constructor.
    Add simple log on test success, or fail.

--- test_package/test_package.c
+++ test_package/test_package.c
@@ -1,4 +1,4 @@
-#include <ktx.h>
+#include "ktx.h"
 
 #include <stdio.h>
 
@@ -10,10 +10,16 @@ int main(int argc, char **argv)
     }
 
     ktxTexture* texture;
-    KTX_error_code result;
+    ktx_error_code_e result;
     result = ktxTexture_CreateFromNamedFile(argv[1],
                                             KTX_TEXTURE_CREATE_LOAD_IMAGE_DATA_BIT,
                                             &texture);
+    if (result == KTX_SUCCESS) {
+        fprintf(stdout, "%s\n", "Success!");
+    } else {
+        fprintf(stderr, "Failed with code %d\n", result);
+        fprintf(stderr, "Check documentation for error codes");
+    }
 
     return 0;
 }
